355167c69b08ccd8b33dbb6308df6a44fb0b955e
Alexander Bail <alexanderbail65@gmail.com>
2022-05-11 20:27:18 +0200

    fix hibernate lazy loading issue

--- src/main/java/de/bail/classicmodels/model/enities/Customer.java
+++ src/main/java/de/bail/classicmodels/model/enities/Customer.java
@@ -46,7 +46,7 @@ public class Customer implements Contact, GenericEntity,  Serializable {
 
   private String country;
 
-  @ManyToOne(fetch = FetchType.EAGER)
+  @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "salesRepEmployeeNumber")
   private Employee salesRepEmployee;
 
